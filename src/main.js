cc6cf7a19e7a4578f7b6fbcd8514fe9d72b2e268
juspher20 <juspher.balangyao@student.passerellesnumeriques.org>
2023-06-20 08:51:34 -0700

    add plugins and vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+
+import '@mdi/font/css/materialdesignicons.css' /** mdi font */
+import { vuetify } from './plugins/vuetify' /** import vuetify */
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify) /** add vuetify */
 
 app.mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+export const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
+})
