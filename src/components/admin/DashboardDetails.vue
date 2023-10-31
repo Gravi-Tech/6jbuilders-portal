620a466a56810af66670dd3479f46d6ca4a3beed
juspher20 <juspher.balangyao@student.passerellesnumeriques.org>
2023-10-31 19:11:14 +0800

    admin dashboard and request pages

--- src/components/admin/DashboardDetails.vue
+++ src/components/admin/DashboardDetails.vue
@@ -1,3 +1,440 @@
 <template>
-  <h1>Welcome to the Dashboard!</h1>
+  <div class="dashboard">
+    <header class="header">
+      <h1 class="dashboard-title">Dashboard</h1>
+    </header>
+    <div class="card-row">
+      <v-card class="overview-card">
+        <v-icon start icon="mdi-label" color="blue" size="x-large"></v-icon>
+        <v-card-title>Total Materials</v-card-title>
+        <v-card-text>
+          <div class="total">
+            {{ totalMaterials }}
+          </div>
+          <v-icon class="icon">mdi-package-variant</v-icon
+          ><v-chip
+            class="link"
+            href="#"
+            size="x-small"
+            color="blue"
+            variant="tonal"
+            text-color="white"
+            style="margin-left: 5px"
+            >View material page</v-chip
+          >
+        </v-card-text>
+      </v-card>
+      <v-card class="overview-card">
+        <v-icon start icon="mdi-label" color="blue" size="x-large"></v-icon>
+        <v-card-title>Total Booking</v-card-title>
+        <v-card-text>
+          <div class="total">{{ totalRequests }}</div>
+          <v-icon class="icon">mdi-calendar-check</v-icon
+          ><v-chip
+            class="link"
+            href="#"
+            size="x-small"
+            color="blue"
+            variant="tonal"
+            text-color="white"
+            style="margin-left: 5px"
+            >View Request page</v-chip
+          >
+        </v-card-text>
+      </v-card>
+      <v-card class="overview-card">
+        <v-icon start icon="mdi-label" color="blue" size="x-large"></v-icon>
+        <v-card-title>Clients</v-card-title>
+        <v-card-text>
+          <div class="total">{{ totalClients }}</div>
+          <v-icon class="icon">mdi-account-group</v-icon
+          ><v-chip
+            class="link"
+            href="#"
+            size="x-small"
+            color="blue"
+            variant="tonal"
+            text-color="white"
+            style="margin-left: 5px"
+            >View material page</v-chip
+          >
+        </v-card-text>
+      </v-card>
+      <v-card class="overview-card">
+        <v-icon start icon="mdi-label" color="blue" size="x-large"></v-icon>
+        <v-card-title>Remaining Task</v-card-title>
+        <v-card-text>
+          <div class="total">{{ totalRemainingTasks }}</div>
+          <v-icon class="icon">mdi-checkbox-multiple-outline</v-icon
+          ><v-chip
+            class="link"
+            href="#"
+            size="x-small"
+            color="blue"
+            variant="tonal"
+            text-color="white"
+            style="margin-left: 5px"
+            >View material page</v-chip
+          >
+        </v-card-text>
+      </v-card>
+      <v-card class="overview-card">
+        <v-icon start icon="mdi-label" color="blue" size="x-large"></v-icon>
+        <v-card-title>Workers</v-card-title>
+        <v-card-text>
+          <div class="total">{{ totalWorkers }}</div>
+          <v-icon class="icon">mdi-account-multiple</v-icon
+          ><v-chip
+            class="link"
+            href="#"
+            size="x-small"
+            color="blue"
+            variant="tonal"
+            text-color="white"
+            style="margin-left: 5px"
+            >View material page</v-chip
+          >
+        </v-card-text>
+      </v-card>
+      <v-card class="overview-card">
+        <v-icon start icon="mdi-label" color="blue" size="x-large"></v-icon>
+        <v-card-title>Revenue</v-card-title>
+        <v-card-text>
+          <div style="display: flex; justify-content: center; align-items: center">
+            <v-icon class="icon">mdi-currency-php</v-icon>
+            <div class="total revenue">{{ totalRevenue }}</div>
+          </div>
+        </v-card-text>
+      </v-card>
+    </div>
+    <div class="sub-cards">
+      <v-card class="new-booking-card">
+        <v-container>
+          <div class="head">
+            <v-icon color="blue" size="large">mdi-calendar-check</v-icon>
+            <v-card-title class="table-header"> Newly Requested </v-card-title>
+          </div>
+
+          <v-card-text>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Booking ID</th>
+                  <th>Full Name</th>
+                  <th>Mobile Number</th>
+                  <th>Service</th>
+                  <th>Created On</th>
+                  <th>Location</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="row in latestBookings" :key="row.id">
+                  <td>
+                    <a :href="getDetailsPageUrl(row.id)" style="color: blue">{{
+                      shortenId(row.id)
+                    }}</a>
+                  </td>
+                  <td>{{ row.fullName }}</td>
+                  <td>{{ row.mobileNumber }}</td>
+                  <td>{{ row.service }}</td>
+                  <td>{{ row.createdDate }}</td>
+                  <td>{{ row.location }}</td>
+                  <td :class="getStatusClass(row.status)">{{ row.status }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </v-card-text>
+        </v-container>
+      </v-card>
+
+      <v-card class="new-notification-card">
+        <v-container>
+          <div class="head">
+            <v-icon color="blue" size="large">mdi-bell</v-icon>
+            <v-card-title class="table-header">To Visit Location</v-card-title>
+          </div>
+          <template v-for="(notification, index) in displayedNotifications" :key="index">
+            <v-card variant="tonal" class="notification-card">
+              <v-card-text>
+                <div>
+                  <h5>{{ notification.message }}</h5>
+                </div>
+                <v-divider :thickness="2" class="border-opacity-100" color="info"></v-divider>
+                <div class="notification-footer">
+                  <v-chip
+                    prepend-icon="mdi-eye"
+                    size="small"
+                    variant="outlined"
+                    color="info"
+                    @click="viewDetails(notification)"
+                    >View Details</v-chip
+                  >
+                  <v-chip
+                    prepend-icon="mdi-calendar"
+                    size="small"
+                    variant="outlined"
+                    color="success"
+                  >
+                    {{ notification.createdDate }}
+                  </v-chip>
+                </div>
+              </v-card-text>
+            </v-card>
+          </template>
+        </v-container>
+      </v-card>
+    </div>
+  </div>
 </template>
+
+<script>
+import {
+  pendingData,
+  completedData,
+  ongoingData,
+  materialData,
+  workersData
+} from '../../utils/tableData'
+
+export default {
+  data() {
+    return {
+      bookingRequests: pendingData,
+      completedRequests: completedData,
+      onGoingRequests: ongoingData,
+      materials: materialData,
+      workers: workersData,
+      selectedFilter: 'All Service',
+      selectedRowFilter: 'all',
+      showAlert: false,
+      alertTimeout: null,
+      displayedNotifications: [],
+      maxDisplayCount: 4
+    }
+  },
+  mounted() {
+    this.updateDisplayedNotifications()
+  },
+  computed: {
+    totalMaterials() {
+      return this.materials.reduce((total, material) => total + material.quantity, 0)
+    },
+    totalRequests() {
+      return (
+        this.completedRequests.length + this.onGoingRequests.length + this.bookingRequests.length
+      )
+    },
+    totalClients() {
+      return this.bookingRequests.length
+    },
+    totalRemainingTasks() {
+      return this.onGoingRequests.length
+    },
+    totalWorkers() {
+      return this.workers.length
+    },
+    totalRevenue() {
+      const revenue = this.completedRequests.reduce((total, project) => {
+        const amount = parseFloat(project.total_amount.replace(',', ''))
+        return total + amount
+      }, 0)
+      return `${revenue.toFixed(2)}`
+    },
+    latestBookings() {
+      const sortedBookings = this.bookingRequests.sort(
+        (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+      )
+      return sortedBookings.slice(0, 12)
+    }
+  },
+  methods: {
+    shortenId(id) {
+      return id.substring(0, 8)
+    },
+    getDetailsPageUrl(id) {
+      return '/6jbuilders/admin/request?id=' + id
+    },
+    getStatusClass(status) {
+      switch (status) {
+        case 'Pending':
+          return 'status status--pending'
+        case 'Accepted':
+          return 'status status--accepted'
+        case 'Rejected':
+          return 'status status--rejected'
+        default:
+          return 'status'
+      }
+    },
+    updateDisplayedNotifications() {
+      const currentDate = new Date()
+
+      const filteredRequests = this.bookingRequests
+        .filter((request) => {
+          const scheduleDate = new Date(request.scheduleDate)
+          return scheduleDate >= currentDate && !request.isVisited
+        })
+        .sort((a, b) => new Date(a.scheduleDate) - new Date(b.scheduleDate))
+
+      this.displayedNotifications = filteredRequests
+        .slice(0, this.maxDisplayCount)
+        .map((request) => ({
+          id: request.id,
+          scheduleDate: request.scheduleDate,
+          selectedTimeRange: request.selectedTimeRange,
+          location: request.location,
+          createdDate: request.createdDate,
+          message: `The pending request with ID ${request.id} is scheduled for inspection on ${request.scheduleDate} ${request.selectedTimeRange} at ${request.location}.`
+        }))
+    },
+    viewDetails(notification) {}
+  }
+}
+</script>
+
+<style scoped>
+.dashboard {
+  padding: 24px;
+}
+
+.header {
+  margin-bottom: 24px;
+}
+
+.dashboard-title {
+  font-size: 24px;
+  font-weight: 700;
+}
+
+.card-row {
+  display: flex;
+  gap: 16px;
+  margin-bottom: 24px;
+}
+
+.overview-card {
+  flex: 1;
+}
+.sub-cards {
+  display: flex;
+}
+
+.head {
+  display: flex;
+  align-items: center;
+}
+
+.new-booking-card {
+  width: 900px;
+  margin: 0 10px 24px 0;
+}
+
+.new-notification-card {
+  width: 410px;
+  margin: 0 0 24px 10px;
+}
+.new-booking-card,
+.new-notification-card {
+  border-radius: 4px;
+  border: 1px solid #ddd;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.table-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: rgba(43, 146, 249, 0.958);
+  font-weight: 700;
+  font-size: 18px;
+}
+
+.filter-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.filter-select {
+  width: 400px;
+}
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table th,
+.table td {
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+  text-align: left;
+}
+
+.table th {
+  font-weight: bold;
+}
+
+.table tbody tr:nth-child(even) {
+  background-color: #e0e0e0;
+}
+
+.table tbody tr:nth-child(odd) {
+  background-color: #ffffff;
+}
+
+.table tbody td:last-child {
+  white-space: nowrap;
+}
+
+.status {
+  display: inline-block;
+  padding: 4px 8px;
+  color: #fff;
+}
+
+.status {
+  font-size: 14px;
+  font-weight: 500;
+}
+
+.status--pending {
+  color: orange;
+}
+
+.status--rejected {
+  color: red;
+}
+
+.status--accepted {
+  color: green;
+}
+
+.total {
+  text-align: center;
+  font-size: 32px;
+  font-weight: 600;
+  padding: 20px;
+}
+
+.revenue {
+  color: green;
+}
+
+.link {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.notification-footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+}
+
+.notification-card {
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/utils/tableData.js
@@ -0,0 +1,489 @@
+export const pendingData = [
+  {
+    id: '653f74f8d58daf137c3c3a16',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: 'jus@gmail.com',
+    service: 'Drywall Installation',
+    location: 'Cebu City',
+    createdDate: '2023-09-02',
+    scheduleDate: '2023-11-01',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: true,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo.'
+  },
+  {
+    id: '653f840df315b80c420419e6',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Drywall Installation',
+    location: 'Cebu City',
+    createdDate: '2023-09-10',
+    scheduleDate: '2023-11-02',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo.'
+  },
+  {
+    id: '653f84d6df17064b71d90f9d',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: 'created@gmail.com',
+    service: 'Drywall Installation',
+    location: 'Cebu City',
+    createdDate: '2022-08-01',
+    scheduleDate: '2023-11-02',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo.'
+  },
+  {
+    id: '653f84d6df17064b71d90f9f',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Home Renovation',
+    location: 'Cebu City',
+    createdDate: '2023-08-01',
+    scheduleDate: '2023-9-21',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo.'
+  },
+  {
+    id: '653f84d6df17064b71d90f12',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: 'email@gmail.com',
+    service: 'Drywall Installation',
+    location: 'Cebu City',
+    createdDate: '2023-09-01',
+    scheduleDate: '2023-11-11',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo.'
+  },
+  {
+    id: '653f84d6df17064b71d9f10f',
+    type: 'Private Individual',
+    fullName: 'Juspher Balangyao',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Home Renovation',
+    location: 'Cebu City',
+    createdDate: '2023-09-12',
+    scheduleDate: '2023-11-02',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo.'
+  },
+  {
+    id: '653f84d6df17064b71d90f1r',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Drywall Installation',
+    location: 'Cebu City',
+    createdDate: '2023-09-01',
+    scheduleDate: '2023-10-01',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo.'
+  },
+  {
+    id: '653f84d6df17064b71d90fff',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Home Renovation',
+    location: 'Cebu City',
+    createdDate: '2023-09-01',
+    scheduleDate: '2023-10-12',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: ''
+  },
+  {
+    id: '653f84d6df17064b71d90f2f',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Drywall Installation',
+    location: 'Cebu City',
+    createdDate: '2023-09-01',
+    scheduleDate: '2023-10-01',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: ''
+  },
+  {
+    id: '653f84d6df17064b71d90f5g',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Home Renovation',
+    location: 'Cebu City',
+    createdDate: '2023-09-01',
+    scheduleDate: '2023-10-01',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: ''
+  },
+  {
+    id: '653f84d6df17064b71d90f0g',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Home Renovation',
+    location: 'Cebu City',
+    createdDate: '2023-09-01',
+    scheduleDate: '2023-10-01',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: ''
+  },
+  {
+    id: '653f84d6df17064b71d90f7g',
+    type: 'Private Individual',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    email: '',
+    service: 'Home Renovation',
+    location: 'Cebu City',
+    createdDate: '2023-09-01',
+    scheduleDate: '2023-10-01',
+    selectedTimeRange: '02:00 PM - 04:00 PM',
+    isVisited: false,
+    zipCode: '6001',
+    attachment: '',
+    status: 'Pending',
+    note: ''
+  }
+]
+
+export const completedData = [
+  {
+    id: '617e9b2c6e2d647f28b062t4',
+    booking_id: '617e9b2c6e2d647f28b062u2',
+    service: 'Home Renovation',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    inspection_date: '2023-6-01',
+    isVisited: true,
+    status: 'Completed',
+    total_material_cost: '650,000.00',
+    total_amount: '1,000,000.00',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      }
+    ]
+  },
+  {
+    id: '617e9b2c6e2d647f28b062t3',
+    booking_id: '617e9b2c6e2d647f28b062u3',
+    service: 'Home Renovation',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    isVisited: true,
+    status: 'Completed',
+    total_material_cost: '650,000.00',
+    total_amount: '1,000,000.00',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      }
+    ]
+  },
+  {
+    id: '617e9b2c6e2d647f28b062t2',
+    booking_id: '617e9b2c6e2d647f28b062u4',
+    service: 'Home Renovation',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    isVisited: true,
+    status: 'Completed',
+    total_material_cost: '650,000.00',
+    total_amount: '1,000,000.00',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      }
+    ]
+  },
+  {
+    id: '617e9b2c6e2d647f28b062t1',
+    booking_id: '617e9b2c6e2d647f28b062u5',
+    service: 'Home Renovation',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    isVisited: true,
+    status: 'Completed',
+    total_material_cost: '650,000.00',
+    total_amount: '1,500,000.00',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      }
+    ]
+  }
+]
+
+export const ongoingData = [
+  {
+    id: '617e9b2c6e2d647f28b062c2',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    service: 'Cabinets',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    isVisited: true,
+    address: 'Address A',
+    status: 'Ongoing',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062d4'
+      }
+    ]
+  },
+
+  {
+    id: '617e9b2c6e2d647f28b062c3',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    service: 'Roofing',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    isVisited: true,
+    address: 'Address A',
+    status: 'Ongoing',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062d4'
+      }
+    ]
+  },
+  {
+    id: '617e9b2c6e2d647f28b062c4',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    service: 'Electrical',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    isVisited: true,
+    address: 'Address A',
+    status: 'Ongoing',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062d4'
+      }
+    ]
+  },
+
+  {
+    id: '617e9b2c6e2d647f28b062c5',
+    fullName: 'John Doe',
+    mobileNumber: '0999999999',
+    service: 'Home Renovation',
+    location: 'Location A',
+    date_created: '2023-6-04',
+    date_updated: '2023-8-01',
+    date_started: '2023-8-01',
+    date_ended: '2023-10-01',
+    inspection_date: '2023-6-01',
+    isDeleted: false,
+    isVisited: true,
+    address: 'Address A',
+    status: 'Ongoing',
+    workers: [
+      {
+        id: '617e9b2c6e2d647f28b062c8'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062c9'
+      },
+      {
+        id: '617e9b2c6e2d647f28b062d4'
+      }
+    ]
+  }
+]
+
+export const materialData = [
+  {
+    id: '617e9b2c6e2d647f28b062c6',
+    name: 'Drywall Sheets',
+    quantity: 100,
+    unit: 'pcs',
+    cost: '500.00'
+  },
+  {
+    id: '617e9b2c6e2d647f28b062c7',
+    name: 'Paint Buckets',
+    quantity: 102,
+    unit: 'pcs',
+    cost: '100.00'
+  }
+]
+
+export const workersData = [
+  {
+    id: '617e9b2c6e2d647f28b062c8',
+    name: 'John Doe',
+    position: 'Foreman',
+    department: 'Construction',
+    experience: '10 years',
+    specializations: ['Carpentry', 'Masonry']
+  },
+  {
+    id: '617e9b2c6e2d647f28b062c9',
+    name: 'Jane Smith',
+    position: 'Equipment Operator',
+    department: 'Construction',
+    experience: '5 years',
+    specializations: ['Excavation', 'Heavy machinery']
+  },
+  {
+    id: '617e9b2c6e2d647f28b062d1',
+    name: 'Michael Johnson',
+    position: 'Laborer',
+    department: 'Construction',
+    experience: '2 years',
+    specializations: ['Demolition', 'Concrete work']
+  },
+  {
+    id: '617e9b2c6e2d647f28b062d2',
+    name: 'Robert Williams',
+    position: 'Plumber',
+    department: 'Construction',
+    experience: '8 years',
+    specializations: ['Pipe fitting', 'Plumbing installations']
+  },
+  {
+    id: '617e9b2c6e2d647f28b062d3',
+    name: 'Emily Davis',
+    position: 'Electrician',
+    department: 'Construction',
+    experience: '6 years',
+    specializations: ['Wiring', 'Electrical installations']
+  },
+  {
+    id: '617e9b2c6e2d647f28b062d4',
+    name: 'David Brown',
+    position: 'Painter',
+    department: 'Construction',
+    experience: '4 years',
+    specializations: ['Interior painting', 'Exterior painting']
+  }
+]
