eaa18d009447921caaef733c62104216221b61c3
Juspher Estoque Balangyao <95953028+juspher20@users.noreply.github.com>
2023-11-13 03:29:52 +0000

    worker page

---
+++ src/dataUtils/positionType.js
@@ -0,0 +1,51 @@
+export const positionTypes = [
+    {
+        id: '1e80d0a6-82e5-4e7a-93d5-954fde8a7bee',
+        title: 'All',
+        short_title: 'all',
+        createdDate: '2021-01-01',
+        updatedDate: '2021-01-01'
+    },
+    {
+        id: '1e80d0a6-82e5-4e7a-93d5-954fde8a7b1a',
+        title: 'Equipment Operator',
+        short_title: 'eqop',
+        createdDate: '2021-01-01',
+        updatedDate: '2021-01-01'
+    },
+    {
+        id: 'fdb8b7d5-6d4c-4b13-8f2a-9d3c8cf3c4a7',
+        title: 'Laborer',
+        short_title: 'lab',
+        createdDate: '2021-01-02',
+        updatedDate: '2021-01-02'
+    },
+    {
+        id: '4683e4f3-1b45-4cd8-b423-6b7e2d2c5d9a',
+        title: 'Plumber',
+        short_title: 'plum',
+        createdDate: '2021-01-03',
+        updatedDate: '2021-01-03'
+    },
+    {
+        id: '0df8b2c3-6d2e-4e5e-9a0f-02efbcf7e9d1',
+        title: 'Electrician',
+        short_title: 'elec',
+        createdDate: '2021-01-04',
+        updatedDate: '2021-01-04'
+    },
+    {
+        id: '837e8e5a-3a6d-4f2f-8ebe-4d5a5f4f654d',
+        title: 'Painter',
+        short_title: 'pai',
+        createdDate: '2021-01-05',
+        updatedDate: '2021-01-05'
+    },
+    {
+        id: 'f3a5d4b6-8a9b-4c0d-9f7e-6d2e1b3c0a9a',
+        title: 'Welder',
+        short_title: 'weld',
+        createdDate: '2021-01-10',
+        updatedDate: '2021-01-10'
+    }
+]
