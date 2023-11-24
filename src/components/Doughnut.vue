fbfa5d61a6133fb70de46068b6bd47a561a0598d
Dev-Opora <122242901+Dev-Opora@users.noreply.github.com>
2023-11-24 13:46:32 +0800

    graphs in dashboard

---
+++ src/components/Doughnut.vue
@@ -0,0 +1,29 @@
+<template>
+      <Doughnut :data="chartData" :options="chartOptions"/>
+</template>
+<script>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+export default {
+  data: () => ({
+    chartData: {
+      labels: ['pending', 'on going', 'completed' , 'to be visit'],
+      datasets: [
+        {
+          backgroundColor: ['#00D8FF', '#E46651', '#35495E', '#DD1B16'],
+          data: [40, 20, 80, 10]
+        }
+      ]
+    },
+    chartOptions:{
+      responsive: true
+    }
+  }), 
+  components:{
+      Doughnut,
+  }
+}
+</script>
+<style></style>
